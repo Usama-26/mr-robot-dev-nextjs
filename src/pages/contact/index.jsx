d6de1b8d8fd1261d0b91cd5f71df5270cd52726c
Usama Ali <75754522+Usama-26@users.noreply.github.com>
2023-05-11 20:51:14 +0500

    Map Added

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -3,6 +3,8 @@ import MobileFooter from "@/components/MobileFooter";
 import Navbar from "@/components/Navbar";
 import useMediaQuery from "@/hooks/useMediaQuery";
 import Image from "next/image";
+import { AiFillHome } from "react-icons/ai";
+import { MdLocalPhone, MdEmail, MdLocationOn } from "react-icons/md";
 
 export default function Contact() {
   const isMobileScreen = useMediaQuery("(max-width: 640px");
@@ -79,7 +81,40 @@ export default function Contact() {
             </button>
           </div>
         </form>
+
+        <div className="flex flex-col md:flex-row gap-10">
+          <div className="rounded-3xl bg-black flex flex-col gap-10 basis-2/5 px-20 py-12">
+            <div className="flex items-center gap-10 mb-10">
+              <AiFillHome className="w-12 h-12 fill-primary-red" />
+              <h1 className="text-3xl font-bold">
+                Get in <span className="text-primary-red"> Touch</span>
+              </h1>
+            </div>
+            <div className="flex items-center gap-10">
+              <MdLocalPhone className="w-12 h-12 fill-primary-red" />
+              <h1 className="text-2xl">+27 79 957 7606</h1>
+            </div>
+            <div className="flex items-center gap-10">
+              <MdEmail className="w-12 h-12 fill-primary-red" />
+              <h1 className="text-2xl">sales@mrrobot.com</h1>
+            </div>
+            <div className="flex items-center gap-10">
+              <MdLocationOn className="w-12 h-12 fill-primary-red" />
+              <h1 className="text-2xl">Cape town, South Africa</h1>
+            </div>
+          </div>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d633160.1046942765!2d18.221863380387568!3d-33.8654062401274!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1dcc500f8826eed7%3A0x687fe1fc2828aa87!2sCape%20Town%2C%20South%20Africa!5e0!3m2!1sen!2s!4v1683820168501!5m2!1sen!2s"
+            width="600"
+            height="450"
+            allowfullscreen=""
+            loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"
+            className="grayscale basis-3/5 rounded-3xl"
+          ></iframe>
+        </div>
       </div>
+
       {isMobileScreen ? <MobileFooter /> : <Footer />}
     </main>
   );
