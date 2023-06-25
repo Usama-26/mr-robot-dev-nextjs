d41f2bc9edb2391c0d9d3c891380d7124e78ad20
UndergroundProgrammer <88035195+UndergroundProgrammer@users.noreply.github.com>
2023-06-25 22:42:48 +0500

    code merged

--- src/components/Chat/FileMessageWithProgress.jsx
+++ src/components/Chat/FileMessageWithProgress.jsx
@@ -1,15 +1,3 @@
-<<<<<<< Updated upstream
-import React from "react";
-import { useEffect } from "react";
-import { useState } from "react";
-import { BsFileImage } from "react-icons/bs";
-import { IoMdDownload } from "react-icons/io";
-import { uploadImage } from "../FileUploader/ImageUpload";
-import alert from "../Notification/Alert";
-import chatsRepository from "@/repositories/chatsRepository";
-import { saveAs } from "file-saver";
-import moment from "moment";
-=======
 import React from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
@@ -21,42 +9,14 @@ import {
     convertDateAndTime,
     convertMessageToShortName,
 } from '@/helpers/chatHelpers';
->>>>>>> Stashed changes
 
 export function FileMessageWithProgress({
-  socket,
-  chatId,
-  message,
-  file,
-  userId,
+    socket,
+    chatId,
+    message,
+    file,
+    userId,
 }) {
-<<<<<<< Updated upstream
-  const [progress, setProgress] = useState();
-  const uploadFile = async () => {
-    await uploadImage(
-      file,
-      async (url, success) => {
-        if (success) {
-          const newObj = {
-            chatId,
-            messageBody: {
-              ...message,
-              ...{
-                attachment: url,
-              },
-            },
-          };
-          try {
-            const { result } = await chatsRepository.createMessage(newObj);
-            console.log(userId);
-            socket.emit("send-message-staff", userId, result);
-          } catch (error) {
-            alert.showErrorAlert(error);
-          }
-        }
-      },
-      setProgress
-=======
     const [progress, setProgress] = useState();
     const [url, setUrl] = useState('');
     const downlaodFile = (e) => {
@@ -125,38 +85,5 @@ export function FileMessageWithProgress({
                 </div>
             )}
         </div>
->>>>>>> Stashed changes
     );
-  };
-  useEffect(() => {
-    if (file && message.attachment === "attachment") uploadFile();
-  }, []);
-
-  const handleDownload = () => {
-    saveAs(message?.attachment, "image.jpg");
-  };
-
-  return (
-    <div className="w-32">
-      <div className="flex justify-between">
-        <BsFileImage className="inline w-6 h-6 justify-end" />
-        <button onClick={handleDownload}>
-          <IoMdDownload className="inline w-6 h-6 justify-end" />
-        </button>
-      </div>
-
-      <span className="block">{message?.message}</span>
-      <span>{moment(message?.time).format("DD/MM/YYYY HH:mm")}</span>
-      {progress < 100 && (
-        <div className="relative h-2 bg-gray-300">
-          <div
-            className="absolute top-0 left-0 h-2 bg-green-500"
-            style={{
-              width: `${progress}%`,
-            }}
-          ></div>
-        </div>
-      )}
-    </div>
-  );
 }
